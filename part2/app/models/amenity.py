397be0504a7bce83fc102374a133d9094dc1ffc7
HISvessel <kevinsanchezdabest@yahoo.com>
2025-06-18 10:59:51 -0400

    creation of folders for reviews and amenities

---
+++ part2/app/models/amenity.py
@@ -0,0 +1,2 @@
+"""this is the creation of the file for the amenity entity:
+pending further elaboration, since this is for the structure of folders"""
---
+++ part2/app/models/review.py
@@ -0,0 +1,2 @@
+"""this is the creation of the file for the review entity:
+pending edition, since this is for the structure of our files"""
