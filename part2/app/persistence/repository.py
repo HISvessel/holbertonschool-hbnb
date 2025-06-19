48f00456504a801d51d55f86e5ccef4e7bed3eb9
HISvessel <kevinsanchezdabest@yahoo.com>
2025-06-19 00:28:35 -0400

    adding changes and basic function and classes to our repository file

---
+++ part2/app/persistence/repository.py
@@ -0,0 +1,51 @@
+"""creation for the places data storing:
+pending changes, as this is for file structure"""
+from abc import ABC, abstractmethod
+
+
+class Repository(ABC):
+    @abstractmethod
+    def add(self, obj):
+        pass
+
+    @abstractmethod
+    def get(slef, obj_id):
+        pass
+    
+    @abstractmethod
+    def get_all(self):
+        pass
+    
+    @abstractmethod
+    def update(self, obj_id, data):
+        pass
+
+    @abstractmethod
+    def delete(self, obj_id):
+        pass
+
+    @abstractmethod
+    def get_by_attribute(self, attr_name, attr_value):
+        pass
+
+class InMemoryRepository(Repository):
+    def __init__(self):
+        self._storage = {}
+    
+    def get(self, obj_id):
+        return self._storage.get(obj_id)
+    
+    def get_all(self):
+        return list(self._storage.values())
+    
+    def update(self, obj_id, data):
+        obj = self.get(obj_id)
+        if obj:
+            obj.update(data)
+    
+    def delete(self, obj_id):
+        if obj_id in self._storage:
+            del self._storage(obj_id)
+    
+    def get_by_attribute(self, attr_name, attr_value):
+        return next(obj for obj in self._storage.values() if getattr(obj, attr_name) == attr_value)
