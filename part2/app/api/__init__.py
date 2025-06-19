e5c14950fdfedec382492176a68b81498956c28f
HISvessel <kevinsanchezdabest@yahoo.com>
2025-06-19 01:33:56 -0400

    initializer method calls the correct link direction, otherwise it would not start. first debugging, complete

--- part2/app/api/__init__.py
+++ part2/app/api/__init__.py
@@ -3,7 +3,7 @@ from flask_restx import Api
 
 def create_app():
     app = Flask(__name__)
-    api = Api(app, version='1.0', title='Hbnb API', description='Hbnb Application API', doc='api/v1')
+    api = Api(app, version='1.0', title='Hbnb API', description='Hbnb Application API', doc='/api/v1')
 
     #Placeholder for API namespace(endpoints will be added later)
     #additional namespaces for places, reviews, and ammenities will be added later
