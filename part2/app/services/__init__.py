078db7359fc3f025e9ca9c452ef74adf4b30d0b1
HISvessel <kevinsanchezdabest@yahoo.com>
2025-06-19 00:27:20 -0400

    adding proper basic functions and importing modules

--- part2/app/services/__init__.py
+++ part2/app/services/__init__.py
@@ -1,1 +1,5 @@
 """initializer method for our services"""
+from app.services.facade import HbnbFacade
+
+
+facade = HbnbFacade
--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,1 +1,17 @@
 """the integration of our facade, for interaction and connection between entities"""
+from app.persistence import InMemoryRepository
+
+
+class HbnbFacade():
+    def __init__(self):
+        self.user_repo = InMemoryRepository
+        self.place_repo = InMemoryRepository
+        self.review_repo = InMemoryRepository
+        self.amenity_repo = InMemoryRepository
+    
+    #Placeholder method for creating a user
+    def create_user(self, user_data):
+        pass
+
+    def get_place(seld, place_id):
+        pass
