20294169b339bf854006fc7861a8ef9137dc6987
HISvessel <kevinsanchezdabest@yahoo.com>
2025-07-15 18:08:22 -0400

    debugged database entry nd depositing of early data

--- part3/insert_admin_and_amenity.py
+++ part3/insert_admin_and_amenity.py
@@ -1,5 +1,6 @@
 import sqlite3
 from uuid import uuid4
+import os
 from app.models.user import User
 
 initial_amenities = ['WIFi', 'Swimming Pool', 'Air Conditioning']
@@ -7,14 +8,15 @@ initial_amenities = ['WIFi', 'Swimming Pool', 'Air Conditioning']
 ADMIN_ID = "36c9050e-ddd3-4c3b-9731-9f487208bbc1"
 
 def connect_DB():
-    return sqlite3.connect('hbnb.db')
+    db = os.path.abspath(os.path.join(os.path.dirname(__file__), '../part3/app/scripts/hbnb.db'))
+    return sqlite3.connect(db)
 
 def insert_admin(cursor):
     admin = User("Admin", "Hbnb", "admin@hbnb.io", "admin1234", is_admin=True)
     admin.id = ADMIN_ID
 
-    cursor.execute("""INSERT INTO INTO users (id, first_name, last_name, email, password, is_admin)
-        VALUES (?, ?, ?, ?, ?, ?)""", admin.first_name, admin.last_name, admin.email, admin.password, admin.is_admin)
+    cursor.execute("""INSERT INTO users (id, first_name, last_name, email, password, is_admin)
+        VALUES (?, ?, ?, ?, ?, ?)""", (admin.id, admin.first_name, admin.last_name, admin.email, admin.password, admin.is_admin))
     
     print("Administrator added successfully")
 
@@ -22,7 +24,7 @@ def insert_amenity(cursor):
     for name in initial_amenities:
         amenity_id = str(uuid4())
         cursor.execute("""INSERT INTO amenities (id, name) VALUES (?, ?)""",
-                        (amenity_id.name))
+                        (amenity_id, name))
         print(f"Amenity inserted: {name}")
 
 def main():
@@ -36,5 +38,5 @@ def main():
     conn.close()
     print("Seeding successful!")
 
-if __name__ == "__main":
+if __name__ == "__main__":
     main()
