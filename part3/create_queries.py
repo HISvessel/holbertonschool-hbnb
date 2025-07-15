4e8c69cf7e29639e7021f7ca4a1a7bfb3185ca14
HISvessel <kevinsanchezdabest@yahoo.com>
2025-07-15 19:56:11 -0400

    created modular functions for insert queries

---
+++ part3/create_queries.py
@@ -0,0 +1,52 @@
+from app.models.amenity import Amenity
+from app.models.places import Place
+from app.models.review import Review
+from app.models.user import User
+from uuid import uuid4
+
+
+def create_amenity(cursor, amenity):
+    """creates simple amenity with a name and an id"""
+    cursor.execute("""INSERT OR IGNORE INTO amenities (id, name) VALUES (?, ?)""", 
+                   (amenity.id, amenity.name))
+
+def create_place(cursor, place):
+    """creates a place if not exists
+    avoids duplicates with UNIQUE PK constraints"""
+    cursor.execute("""INSERT OR IGNORE INTO places 
+                   (id, title, description, price, latitude, longitude, owner_id) 
+                   VALUES (?, ?, ?, ?, ?, ?, ?)""", 
+                   (place.id,
+                    place.title,
+                    place.description,
+                    place.price,
+                    place.latitude,
+                    place.longitude,
+                    place.owner_id
+                    ))
+    
+def create_review(cursor, review):
+    """creates a review if not exists
+    avoids duplicates with UNIQUE and PK constraints"""
+    cursor.execute("""INSERT OR IGNORE INTO reviews 
+                   (id, title, comment, rating, user_id, place_id)
+                   VALUES (?, ?, ?, ?, ?, ?)""", 
+                   (review.id,
+                    review.title,
+                    review.comment,
+                    review.rating,
+                    review.user_id,
+                    review.place_id))
+
+def create_user(cursor, user):
+    """creates a review if not exists, else ignore
+    avoids duplicates by email with UNIQUE and PK constraints"""
+    cursor.execute("""INSERT OR IGNORE INTO users 
+                   (id, first_name, last_name, email, password, is_admin)
+                   VALUES (?, ?, ?, ?, ?, ?)""",
+                   (user.id,
+                    user.first_name,
+                    user.last_name,
+                    user.email,
+                    user.password,
+                    user.is_admin))
--- part3/insert_admin_and_amenity.py
+++ part3/insert_admin_and_amenity.py
@@ -8,29 +8,36 @@ initial_amenities = ['WIFi', 'Swimming Pool', 'Air Conditioning']
 ADMIN_ID = "36c9050e-ddd3-4c3b-9731-9f487208bbc1"
 
 def connect_DB():
+
+    """taps into the database and stores data which is then
+    opened in the main function"""
     db = os.path.abspath(os.path.join(os.path.dirname(__file__), '../part3/app/scripts/hbnb.db'))
     return sqlite3.connect(db)
 
 def insert_admin(cursor):
-    admin = User("Admin", "Hbnb", "admin@hbnb.io", "admin1234", is_admin=True)
-    admin.id = ADMIN_ID
+    """instances data and adds it with cursor.execute"""
 
-    cursor.execute("""INSERT INTO users (id, first_name, last_name, email, password, is_admin)
-        VALUES (?, ?, ?, ?, ?, ?)""", (admin.id, admin.first_name, admin.last_name, admin.email, admin.password, admin.is_admin))
-    
+    admin = User("Admin", "Hbnb", "admin@hbnb.io", "admin1234", is_admin=True)
+    admin.id = ADMIN_ID #set individually for modularity
+    cursor.execute("""INSERT OR IGNORE INTO users (id, first_name, last_name, email, password, is_admin)
+        VALUES (?, ?, ?, ?, ?, ?)""", 
+        (admin.id, admin.first_name, admin.last_name, admin.email, admin.password, admin.is_admin))
     print("Administrator added successfully")
 
 def insert_amenity(cursor):
+    """inserting of multiple amenities"""
+
     for name in initial_amenities:
         amenity_id = str(uuid4())
-        cursor.execute("""INSERT INTO amenities (id, name) VALUES (?, ?)""",
+        cursor.execute("""INSERT OR IGNORE INTO amenities (id, name) VALUES (?, ?)""",
                         (amenity_id, name))
         print(f"Amenity inserted: {name}")
 
 def main():
-    conn = connect_DB()
-    cursor = conn.cursor()
+    conn = connect_DB() #taps into the database
+    cursor = conn.cursor() #database access handle
 
+    #points to our mode and query on the .execute()
     insert_admin(cursor)
     insert_amenity(cursor)
 
