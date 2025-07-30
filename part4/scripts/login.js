fe9d788308a46c106317674aee9c02f1dcaece6f
HISvessel <kevinsanchezdabest@yahoo.com>
2025-07-30 18:14:27 -0400

    moved documents  to fit in their respective place. Attempted to modify header login button after logging in, code implemented but changes are pending success

--- part4/scripts/login.js
+++ part4/scripts/login.js
@@ -8,36 +8,35 @@ const message = document.getElementById('message');
 
         const email = document.getElementById('email').value.trim()
         const password = document.getElementById('password').value.trim()
+        const userAccountButton = document.getElementById('login-link') //gets the login button
 
         console.log(email, password)
         try {
-            const response = await fetch('https://reqres.in/api/login', {
+            const response = await fetch('http://127.0.0.1:5000/v1/user/login', {
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({ email, password })
+                body: JSON.stringify({ email, password }),
+                //credentials: 'include' //including token for credentials
             });
-            
-            const result = await response.json()
-
-            console.log('raw response:', response)
-            console.log("parsed input:", result)
 
             if(response.ok) {
-                //document.cookie = token=$(response.access_token);
-                //window.location.href = 'index.html';
-                message.textContent = 'Login successful';
-                message.style.color = 'green';
-                console.log('Token:', result.token);
+                const result = await response.json()
+                alert('Login successful')
+                document.cookie = `token=${result.access_token}; path=/`;
+                window.location.href = 'index.html';
+
+                //attempting to convert login button to now say the user's name
+                userAccountButton.innerHTML = `<li>${result.name}</li>`; //does not work yet
             }
             else {
                 message.textContent = `Error type: ${result.error}`;
                 message.style.color = 'red';
-                //alert('Login failed:' + response.statusText)
+                alert('Login failed:' + response.statusText)
             }
         } catch (errors) {
             message.textContent = 'Network Error';
             message.style.color = 'red';
-            console.error('Error kind', errors)
+            console.error(errors)
         }
       });
     }
