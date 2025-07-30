679598ee3827ffdb135da556719789275f7be4ad
HISvessel <kevinsanchezdabest@yahoo.com>
2025-07-30 15:47:58 -0400

    implemented cors, now there can be cross relations

--- part3/run.py
+++ part3/run.py
@@ -2,9 +2,11 @@
 runs the app along with the corresponding files:
 pending tests"""
 from app import create_app
+from flask_cors import CORS
 
 
 app = create_app()
+CORS(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
