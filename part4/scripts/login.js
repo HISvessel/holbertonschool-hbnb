7089b4dda1fd43081fc2392d621138c8eb7d6af1
HISvessel <kevinsanchezdabest@yahoo.com>
2025-07-30 15:44:15 -0400

    added debug messages. confirmed that the payload and the response are recieving and parsing correct info. Proceeding to now test on our own server

--- part4/scripts/login.js
+++ part4/scripts/login.js
@@ -9,31 +9,35 @@ const message = document.getElementById('message');
         const email = document.getElementById('email').value.trim()
         const password = document.getElementById('password').value.trim()
 
+        console.log(email, password)
         try {
             const response = await fetch('https://reqres.in/api/login', {
                 method: 'POST',
-                headers: {'Content-type': 'application/json'},
-                body: JSON.stringify({email, password})
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({ email, password })
             });
             
             const result = await response.json()
 
+            console.log('raw response:', response)
+            console.log("parsed input:", result)
+
             if(response.ok) {
-                document.cookie = token=$(response.access_token);
-                window.location.href = 'index.html';
-                document.textContent = 'Login successful';
+                //document.cookie = token=$(response.access_token);
+                //window.location.href = 'index.html';
+                message.textContent = 'Login successful';
                 message.style.color = 'green';
                 console.log('Token:', result.token);
             }
             else {
-                message.textContent = `Error: ${result.error}`;
+                message.textContent = `Error type: ${result.error}`;
                 message.style.color = 'red';
-                alert('Login failed:' + response.statusText)
+                //alert('Login failed:' + response.statusText)
             }
         } catch (errors) {
             message.textContent = 'Network Error';
             message.style.color = 'red';
-            console.error('Error', errors)
+            console.error('Error kind', errors)
         }
       });
     }
